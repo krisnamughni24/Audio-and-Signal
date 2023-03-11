95665c4f04c743b6b561c33e4aeb080f4eeb76a8
Krisna Mughni <68163712+krisnamughni24@users.noreply.github.com>
2023-03-11 21:34:55 +0700

    add logic

--- 1. basic notes,  tempo, beats, and looping.rb
+++ 1. basic notes,  tempo, beats, and looping.rb
@@ -1,35 +1,36 @@
 use_bpm 60      # tempo
 
-5.times do      # looping 5 times
+3.times do      # looping 3 times
   
   2.times do
-    play 51     # D#3
-    sleep 0.4
-    play 51     # D#3
-    sleep 0.4
+    play 51     # Eb
+    sleep 0.5   # whole note
+    play 51     # Eb
+    sleep 0.45
     play 62     # D4
-    sleep 0.4
+    sleep 0.35
     play 62     # D4
     sleep 0.3
-    play 58     # A#3
-    sleep 0.4
+    play 58     # Bb
+    sleep 0.5   # whole note
   end
   
   2.times do
     play 55     # G3
-    sleep 0.4
+    sleep 0.5   # whole note
     play 55     # G3
-    sleep 0.4
+    sleep 0.45
     play 62     # D4
-    sleep 0.4
+    sleep 0.35
     play 62     # D4
     sleep 0.3
-    play 58     # A#3
-    sleep 0.4
+    play 58     # Bb
+    sleep 0.5   # whole note
   end
   
-  play 60       # c4
-  sleep 0.3
+  play 60       # C4
+  sleep 0.1
+  sleep 0.25    # 1/16 note rest
   
 end
 
